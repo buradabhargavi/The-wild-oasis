a7547841815f6783dac915ccb65f8b8ed689c83f
buradabhargavi <bhargaviboorada246@gmail.com>
2024-07-25 09:01:34 +0530

    backgroundChanges

--- src/ui/Header.js
+++ src/ui/Header.js
@@ -2,9 +2,14 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledHeader = styled.header`
-  background-color: orangered;
+  background-color: var(--color-grey-0);
   padding: 1.2rem 4.8rem;
   border-bottom: 1px solid var(--color-gray);
+
+  display: flex;
+  gap: 2.4rem;
+  align-items: center;
+  justify-content: flex-end;
 `;
 function Header() {
   return <StyledHeader>StyledHeader</StyledHeader>;
